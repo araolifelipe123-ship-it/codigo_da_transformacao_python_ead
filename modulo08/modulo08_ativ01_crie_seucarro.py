a81e7a3a5545dcb26e2693eeb1f579cd8243d7de
nathandiasc <araolifelipe123@gmail.com>
2026-08-05 16:41:10 -0300

    uptade

---
+++ modulo08/modulo08_ativ01_crie_seucarro.py
@@ -0,0 +1,12 @@
+class carro:
+    Qodo test this method
+    def__init__(self,marca, modelo):
+        self.marca = marca
+        self.modelo =  ModuleNotFoundError
+        
+        def exibir_info(self):
+            return f"marca: {self.marca}, modelo:
+        
+# meu_carro = carro ("chevorlet", "camaro")
+meu_carro = carro ("ford", "mustang")
+print(meu_carro.exibir_info())
